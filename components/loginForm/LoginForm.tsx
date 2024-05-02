4aafb4ae26587513262dfcb37184b18eff0bb038
ahmaddeveloper17 <ahmaddeveloper17@gmail.com>
2024-05-02 12:29:01 +0500

    Auth Color constants Done

--- components/loginForm/LoginForm.tsx
+++ components/loginForm/LoginForm.tsx
@@ -12,7 +12,7 @@ export default function LoginForm() {
   return (
     <div>
       <div className="">
-        <h2 className=" text-[#F4F4F4] font-ibm-plex-mono pl-[32px] text-2xl font-medium leading-[39px]">
+        <h2 className=" text-Accent font-ibm-plex-mono pl-[32px] text-2xl font-medium leading-[39px]">
           Email
         </h2>
         <input
@@ -21,11 +21,11 @@ export default function LoginForm() {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           disabled={loading}
-          className=" placeholder-[#F4F4F4] text-[white] pl-[32px] bg-[#232020] mt-[16px] font-ibm-plex-mono text-2xl font-medium leading-[39px] rounded-[30px] border-[5px] bordor-color-[#FF7315] w-[307px] sm:w-[447px] h-[58px] border-[#FF7315]"
+          className=" placeholder-[#F4F4F4] text-Accent pl-[32px] bg-Background mt-[16px] font-ibm-plex-mono text-2xl font-medium leading-[39px] rounded-[30px] border-[5px] bordor-color-Border w-[307px] sm:w-[447px] h-[58px] border-Border"
         />
       </div>
       <div className="">
-        <h2 className=" text-[#F4F4F4] mt-[32px] pl-[32px] font-ibm-plex-mono text-2xl font-medium leading-[39px]">
+        <h2 className=" text-Accent mt-[32px] pl-[32px] font-ibm-plex-mono text-2xl font-medium leading-[39px]">
           Confirm Password
         </h2>
         <input
@@ -34,28 +34,28 @@ export default function LoginForm() {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           disabled={loading}
-          className=" placeholder-[#F4F4F4] text-[white] bg-[#232020] mt-[16px] pl-[32px] font-ibm-plex-mono text-2xl font-medium leading-[39px] rounded-[30px] border-[5px] bordor-color-[#FF7315] w-[307px] sm:w-[447px] h-[58px] border-[#FF7315]"
+          className=" placeholder-[#F4F4F4] text-Accent bg-Background mt-[16px] pl-[32px] font-ibm-plex-mono text-2xl font-medium leading-[39px] rounded-[30px] border-[5px] bordor-color-Border w-[307px] sm:w-[447px] h-[58px] border-Border"
         />
       </div>
       <div className="">
         <Link href="/forgot">
-          <h3 className="font-ibm-mono text-[#F4F4F4] mt-[16px] font-medium text-20 leading-26 text-right">
+          <h3 className="font-ibm-mono text-Accent mt-[16px] font-medium text-20 leading-26 text-right">
             Forget Password
           </h3>
         </Link>
-        <div className="flex sm:flex-row bg-[#232020] rounded-[55px] border-[5px] max-w-[447px] min-w-[200px] h-[auto] border-[#FF7315] gap-3 mt-[64px]">
+        <div className="flex sm:flex-row bg-Background rounded-[55px] border-[5px] max-w-[447px] min-w-[200px] h-[auto] border-Border gap-3 mt-[64px]">
           <Image
             src={Images.googleIcon}
             alt=""
             className="w-8 h-8 md:w-12 md:h-12 ml-[15px] mt-[3px]"
           />
-          <h2 className="rounded-[30px] text-[#FF7315] bg-[#232020] font-ibm-plex-mono text-sm md:text-2xl font-medium leading-[39px] w-full h-full sm:ps-[8px] sm:py-[12px]">
+          <h2 className="rounded-[30px] text-Border bg-Background font-ibm-plex-mono text-sm md:text-2xl font-medium leading-[39px] w-full h-full sm:ps-[8px] sm:py-[12px]">
             Sign In with Google
           </h2>
         </div>
-        <div className="w-[100%] sm:w-[307px] md:w-[447px] border-[#FF7315] h-[63px] mt-[10px] bg-[#FF7315] border-[5px] rounded-[55px] ">
+        <div className="w-[100%] sm:w-[307px] md:w-[447px] border-Border h-[63px] mt-[10px] bg-Border border-[5px] rounded-[55px] ">
           <button
-            className="font-ibm-plex-mono text-30  font-medium leading-39 text-[#232020] py-[12px]  px-[36%] sm:px-[180px]"
+            className="font-ibm-plex-mono text-30  font-medium leading-39 text-Background py-[12px]  px-[36%] sm:px-[180px]"
             onClick={login}
             disabled={loading}
           >
--- components/registerForm/RegisterForm.tsx
+++ components/registerForm/RegisterForm.tsx
@@ -23,7 +23,7 @@ export default function RegisterForm() {
   return (
     <div className="">
       <div className="">
-        <h2 className=" text-[#F4F4F4] mt-[32px] pl-[32px] font-ibm-plex-mono text-2xl font-medium leading-[39px]">
+        <h2 className=" text-Accent mt-[32px] pl-[32px] font-ibm-plex-mono text-2xl font-medium leading-[39px]">
           Name
         </h2>
         <input
@@ -32,11 +32,11 @@ export default function RegisterForm() {
           value={name}
           onChange={(e) => setName(e.target.value)}
           disabled={loading}
-          className=" placeholder-[#F4F4F4] text-[white] bg-[#232020] mt-[16px] pl-[32px] font-ibm-plex-mono text-2xl font-medium leading-[39px] rounded-[30px] border-[5px] bordor-color-[#FF7315] w-[300px] md:w-[447px] h-[58px] border-[#FF7315]"
+          className=" placeholder-Accent text-[white] bg-Background mt-[16px] pl-[32px] font-ibm-plex-mono text-2xl font-medium leading-[39px] rounded-[30px] border-[5px] bordor-color-Border w-[300px] md:w-[447px] h-[58px] border-Border"
         />
       </div>
       <div className="mt-[32px]">
-        <h2 className=" text-[#F4F4F4] font-ibm-plex-mono pl-[32px] text-2xl font-medium leading-[39px]">
+        <h2 className=" text-Accent font-ibm-plex-mono pl-[32px] text-2xl font-medium leading-[39px]">
           Email
         </h2>
         <input
@@ -45,11 +45,11 @@ export default function RegisterForm() {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           disabled={loading}
-          className=" placeholder-[#F4F4F4] text-[white] bg-[#232020] mt-[16px] pl-[32px] font-ibm-plex-mono text-2xl font-medium leading-[39px] rounded-[30px] border-[5px] bordor-color-[#FF7315] w-[300px] md:w-[447px] h-[58px] border-[#FF7315]"
+          className=" placeholder-Accent text-[white] bg-Background mt-[16px] pl-[32px] font-ibm-plex-mono text-2xl font-medium leading-[39px] rounded-[30px] border-[5px] bordor-color-Border w-[300px] md:w-[447px] h-[58px] border-Border"
         />
       </div>
       <div className="">
-        <h2 className=" text-[#F4F4F4] mt-[32px] pl-[32px] font-ibm-plex-mono text-2xl font-medium leading-[39px]">
+        <h2 className=" text-Accent mt-[32px] pl-[32px] font-ibm-plex-mono text-2xl font-medium leading-[39px]">
           Password
         </h2>
         <input
@@ -58,11 +58,11 @@ export default function RegisterForm() {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           disabled={loading}
-          className=" placeholder-[#F4F4F4] text-[#F4F4F4] bg-[#232020] mt-[16px] pl-[32px] font-ibm-plex-mono text-2xl font-medium leading-[39px] rounded-[30px] border-[5px] bordor-color-[#FF7315] w-[300px] md:w-[447px] h-[58px] border-[#FF7315]"
+          className=" placeholder-Accent text-Accent bg-Background mt-[16px] pl-[32px] font-ibm-plex-mono text-2xl font-medium leading-[39px] rounded-[30px] border-[5px] bordor-color-Border w-[300px] md:w-[447px] h-[58px] border-Border"
         />
       </div>
       <div className="">
-        <h2 className=" text-[#F4F4F4] mt-[32px] pl-[32px] font-ibm-plex-mono text-2xl font-medium leading-[39px]">
+        <h2 className=" text-Accent mt-[32px] pl-[32px] font-ibm-plex-mono text-2xl font-medium leading-[39px]">
           Confirm Password
         </h2>
         <input
@@ -71,12 +71,12 @@ export default function RegisterForm() {
           value={confirmPassword}
           onChange={(e) => setConfirmPassword(e.target.value)}
           disabled={loading}
-          className=" placeholder-[#F4F4F4] text-[#F4F4F4] bg-[#232020] mt-[16px] pl-[32px] font-ibm-plex-mono text-2xl font-medium leading-[39px] rounded-[30px] border-[5px] bordor-color-[#FF7315] w-[300px] md:w-[447px] h-[58px] border-[#FF7315]"
+          className=" placeholder-Accent text-Accent bg-Background mt-[16px] pl-[32px] font-ibm-plex-mono text-2xl font-medium leading-[39px] rounded-[30px] border-[5px] bordor-color-Border w-[300px] md:w-[447px] h-[58px] border-Border"
         />
       </div>
       <div className="">
         <div
-          className="flex flex-row items-center bg-[#232020] rounded-[55px] border-[5px] w-[307px] md:w-[447px] h-[72px] border-[#FF7315] gap-3 mt-[64px]"
+          className="flex flex-row items-center bg-Background rounded-[55px] border-[5px] w-[307px] md:w-[447px] h-[72px] border-Border gap-3 mt-[64px]"
           onClick={() => {
             signIn("google");
           }}
@@ -86,13 +86,13 @@ export default function RegisterForm() {
             alt=""
             className="w-8 h-8 md:w-12 md:h-12 ml-[24px]"
           />
-          <h2 className=" rounded-[30px] text-[#FF7315] bg-[#232020] font-ibm-plex-mono text-sm md:text-2xl font-medium leading-[39px] w-full h-full ps-[8px] py-[12px]">
+          <h2 className=" rounded-[30px] text-Border bg-Background font-ibm-plex-mono text-sm md:text-2xl font-medium leading-[39px] w-full h-full ps-[8px] py-[12px]">
             Sign In with Google
           </h2>
         </div>
-        <div className="w-[307px] md:w-[447px] border-[#FF7315] h-[63px] left-496 mt-[10px] bg-[#FF7315] border-[5px] rounded-[55px] ">
+        <div className="w-[307px] md:w-[447px] border-Border h-[63px] left-496 mt-[10px] bg-Border border-[5px] rounded-[55px] ">
           <button
-            className=" font-ibm-plex-mono text-30 font-medium leading-39 text-left text-[#232020] py-[12px] px-[110px] md:px-[180px]"
+            className=" font-ibm-plex-mono text-30 font-medium leading-39 text-left text-Background py-[12px] px-[110px] md:px-[180px]"
             onClick={register}
           >
             <h2>Sign In</h2>
@@ -100,12 +100,12 @@ export default function RegisterForm() {
         </div>
       </div>
       <div className="flex  space-x-4 ">
-        <h2 className="text-[#F4F4F4] text-[13px] md:text-[20px] ">
+        <h2 className="text-Accent text-[13px] md:text-[20px] ">
           Already have an account?{" "}
         </h2>
         <Link href="/signin">
           <button>
-            <h2 className="text-[#FF7315] text-[13px] md:text-[20px] underline">
+            <h2 className="text-Border text-[13px] md:text-[20px] underline">
               {" "}
               Log In
             </h2>
