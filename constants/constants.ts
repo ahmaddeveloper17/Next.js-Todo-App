b06b25145ea1693551b9ee826b7e6e00bf71b6e5
ahmaddeveloper17 <125741237+ahmaddeveloper17@users.noreply.github.com>
2024-05-09 15:02:04 +0500

    Update constants.ts

--- constants/constants.ts
+++ constants/constants.ts
@@ -2,7 +2,7 @@ export const Images = {
   editIcon: require("../public/assets/edit.png"),
   googleIcon: require("../public/assets/google.png"),
   mainIcon: require("../public/assets/icon.png"),
-  logoutIcon: require("../public/assets/logout.png"),
+  logoutIcon: require("../public/assets/Logout.png"),
   ProfileIcon: require("../public/assets/settingProfile.png"),
   settingRedIcon: require("../public/assets/profile.png"),
   settingWhiteIcon: require("../public/assets/settingWhite.png"),
